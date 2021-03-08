760314adc3677f4ad83cdc84325bd8bdbf91f3ee
Amar <amarspggawade@gmail.com>
2021-03-08 00:54:04 -0500

    Remove pod files

--- SoccerStar/Pods/Target Support Files/Pods-SoccerStar-SoccerStarUITests/Pods-SoccerStar-SoccerStarUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SoccerStar_SoccerStarUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SoccerStar_SoccerStarUITests
-@end
--- SoccerStar/Pods/Target Support Files/Pods-SoccerStarTests/Pods-SoccerStarTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SoccerStarTests : NSObject
-@end
-@implementation PodsDummy_Pods_SoccerStarTests
-@end
